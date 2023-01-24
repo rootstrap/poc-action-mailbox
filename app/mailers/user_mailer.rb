c82d0c977702bb3ccb8d9057aabb62faf9c7e915
Claudio Kerekes <claudio.kerekes@rootstrap.com>
2023-01-24 17:43:36 -0500

    update user mail with correct locale

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
   def missing(inbound_email)
     mail to: inbound_email,
-         subject: I18n.t('api.email.user.you_do_not_belong_here')
+         subject: I18n.t('email.user.you_do_not_belong_here')
   end
 end
